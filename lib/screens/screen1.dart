fa4434624022ae170115a45340289fccfcf3e9f3
vengeang <therichdad101@gmail.com>
2021-03-10 15:17:49 +0700

    Add lib/screens/screen1.dart to repo

---
+++ lib/screens/screen1.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Screen 1"),
+          ),
+          // body: SpinKitCircle(color: Colors.amber,),
+        ),
+      )
+    );
+  }
+}
